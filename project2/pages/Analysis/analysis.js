8c9e7f397b15bb27b332f75e4541ee0ef904f914
Annika Pavlovich <apavlovich96@gmail.com>
2021-07-15 11:49:46 -0700

    fixed error

--- project2/pages/Analysis/analysis.js
+++ project2/pages/Analysis/analysis.js
@@ -1,10 +1,10 @@
 //Videogame Analysis
 
-var years = ['2600', '5200', '7800', '3DO', '3DS', 'Aco', 'ACPC', 'AJ', 'Amig', 'And', 'ApII', 'Arc', 'AST', 'BBCM', 'BRW', 'C128', 'C64', 'CD32', 'CDi', 'DC', 'DS', 'DSi', 'DSiW', 'FMT', 'GB', 'GBA', 'GBC', 'GC', 'GEN', 'GG', 'GIZ', 'Int', 'iOS', 'iQue', 'Linux', 'Lynx', 'Mob', 'MS', 'MSD', 'MSX', 'N64', 'NES', 'NG', 'NGage', 'NS', 'OSX', 'Ouya']
+var platforms = ['2600', '5200', '7800', '3DO', '3DS', 'Aco', 'ACPC', 'AJ', 'Amig', 'And', 'ApII', 'Arc', 'AST', 'BBCM', 'BRW', 'C128', 'C64', 'CD32', 'CDi', 'DC', 'DS', 'DSi', 'DSiW', 'FMT', 'GB', 'GBA', 'GBC', 'GC', 'GEN', 'GG', 'GIZ', 'Int', 'iOS', 'iQue', 'Linux', 'Lynx', 'Mob', 'MS', 'MSD', 'MSX', 'N64', 'NES', 'NG', 'NGage', 'NS', 'OSX', 'Ouya']
 
 Plotly.d3.csv('https://raw.githubusercontent.com/apavlovich/apavlovich.github.io/main/project2/resources/vgsales-12-4-2019-short.csv',
 (err, rows) => {
-    var data = Platform.map(y => {
+    var data = platforms.map(y => {
         var d = rows.filter(r => r.Platform === y)
 
         return {
